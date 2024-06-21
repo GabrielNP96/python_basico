507994db2c274c58fcbcd2bc4b1a6f22854f8064
GabrielNP96 <gabrielnp1996@gmail.com>
2024-06-21 04:44:28 -0300

    tipo de dados

---
+++ 03_data_types/01_data_types.py
@@ -0,0 +1,11 @@
+name = 'john' #str
+integer = 5 #int
+float_number = 2.5 #float
+boolean = True #bool
+none_type = None #none
+
+print(type(name))
+print(type(integer))
+print(type(float_number))
+print(type(boolean))
+print(type(none_type))
---
+++ 03_data_types/02_specifying_the_data_type.py
@@ -0,0 +1,5 @@
+# Em python é possível especificar o tipo de dados, da seguinte forma:
+name = str('Billy')
+age = int(2)
+
+print(name, age)
