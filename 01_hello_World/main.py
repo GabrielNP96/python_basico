cad6b47225bfdc779897ab725dd488c8d87e73cf
GabrielNP96 <gabrielnp1996@gmail.com>
2024-06-21 04:09:17 -0300

    hello world

---
+++ 01_hello_World/main.py
@@ -0,0 +1,7 @@
+import sys
+#print escreve no console e com a biblioteca sys, é possivel checar a versão do python.
+print('hello world')
+print(sys.version)
+"""
+Assim é possivel criar um comentario de varias linhas
+"""
