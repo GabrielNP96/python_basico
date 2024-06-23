a08dc86711a04725a43b4f8b222cb27a078c63bf
GabrielNP96 <gabrielnp1996@gmail.com>
2024-06-23 16:52:57 -0300

    set

---
+++ 03_data_types/04_set/01_set.py
@@ -0,0 +1,7 @@
+#Um conjunto é uma coleção não ordenada, imutável* e não indexada
+
+my_set = {'Betim','Esmeraldas','Belo Horizonte','Juatuba','Betim'}
+print(my_set)
+
+for x in my_set:
+    print(x)
