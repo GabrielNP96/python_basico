02e8f18720b3b9ae507cf49d58d9de14a38391c3
GabrielNP96 <gabrielnp1996@gmail.com>
2024-06-24 05:08:06 -0300

    functions

---
+++ 06_functions/01_functions.py
@@ -0,0 +1,10 @@
+#criando uma função
+def hello_world():
+    print('Hello world!')
+#chamando uma função
+hello_world()
+#usando return e argumentos
+def soma(num1,num2):
+    return print(f'{num1} + {num2} = {num1 + num2}')
+
+soma(4,10)
