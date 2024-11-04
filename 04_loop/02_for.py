e98c94e83aeb82980e2c59871ef4adadc7b7dffb
GabrielNP96 <gabrielnp1996@gmail.com>
2024-11-03 22:03:24 -0300

    aprendendo sobre for

--- 04_loop/02_for.py
+++ 04_loop/02_for.py
@@ -20,6 +20,7 @@ print("-" * 15)
 for i in range(0,101,10):
     print(i)
 
+print("-" * 15)
 
 soma = 0
 for i in range(1,21,1):
@@ -27,3 +28,13 @@ for i in range(1,21,1):
         soma += i
 
 print(soma) 
+
+print("-" * 15)
+
+soma_multiplos_de_tres = 0
+
+for i in range(1,31,1):
+    if i % 3 == 0:
+        soma_multiplos_de_tres += i
+
+print(soma_multiplos_de_tres)
