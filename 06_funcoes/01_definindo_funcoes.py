b240a9ae3e17d02ced98c9e8a592ccd7715d7fed
GabrielNP96 <gabrielnp1996@gmail.com>
2024-11-04 23:55:00 -0300

    aprendendo sobre funções

---
+++ 06_funcoes/01_definindo_funcoes.py
@@ -0,0 +1,26 @@
+#Definindo uma função simples que soma dois números:
+def soma(a,b):
+    print(f'{a} + {b} = {a+b}')
+
+soma(1,4)
+print('-' * 20)
+
+#Definindo uma função que multiplica dois números:
+def multiplica(a,b):
+    print(f'{a} * {b} = {a*b}')
+
+multiplica(5,2)
+multiplica(4,9)
+multiplica(2,7)
+multiplica(8,2)
+print('-' * 20)
+
+'''Tente criar uma função que calcule o preço total de um item com um desconto padrão de 5%. 
+Assim, se o desconto não for informado, ele usa o valor padrão.'''
+
+def valor_item(a, b=5):
+    print(f'Valor do item: R${a}\nValor do desconto: {b}%.\nValor do item com desconto: R${a - ((a/100) * b)}')
+
+valor_item(29.5)
+valor_item(115, 8)
+print('-' * 20)
